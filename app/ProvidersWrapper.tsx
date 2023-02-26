88a3610d86e73dc7604e2781e3f4895636de18e9
Yaseen Mustapha <yaseenmust@gmail.com>
2023-02-26 15:33:08 -0600

    Implement dark mode using next-themes

--- app/ProvidersWrapper.tsx
+++ app/ProvidersWrapper.tsx
@@ -1,6 +1,21 @@
 "use client";
-import { NextUIProvider } from "@nextui-org/react";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+const lightTheme = createTheme({
+  type: "light",
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+  theme: {
+    colors: {
+      gradient:
+        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
+    },
+  },
+});
 
 export default function ProvidersWrapper({
   children,
@@ -9,7 +24,16 @@ export default function ProvidersWrapper({
 }) {
   return (
     <SessionProvider>
-      <NextUIProvider>{children}</NextUIProvider>
+      <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className,
+        }}
+      >
+        <NextUIProvider>{children}</NextUIProvider>
+      </NextThemesProvider>
     </SessionProvider>
   );
 }
