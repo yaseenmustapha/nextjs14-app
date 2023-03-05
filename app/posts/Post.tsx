24750e1d1b944da5ecb0dfa7f0fe780061be1592
Yaseen Mustapha <yaseenmust@gmail.com>
2023-03-05 02:03:05 -0600

    Style like button

--- app/posts/Post.tsx
+++ app/posts/Post.tsx
@@ -3,20 +3,24 @@ import { useSession } from "next-auth/react";
 import { formatDate } from "@/lib/utils";
 import { Avatar, Card, Grid, Spacer, Text } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const HeartIcon = ({ fill, onClick }: { fill: boolean; onClick: Function }) => {
+  const [hover, setHover] = useState(false);
   return (
     <svg
       width={24}
       height={24}
       viewBox="0 0 24 24"
-      fill={fill ? "red" : "none"}
+      fill={fill ? "#F31260" : "none"}
       xmlns="http://www.w3.org/2000/svg"
       onClick={() => onClick()}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
     >
       <path
         d="M12.62 20.81c-.34.12-.9.12-1.24 0C8.48 19.82 2 15.69 2 8.69 2 5.6 4.49 3.1 7.56 3.1c1.82 0 3.43.88 4.44 2.24a5.53 5.53 0 0 1 4.44-2.24C19.51 3.1 22 5.6 22 8.69c0 7-6.48 11.13-9.38 12.12Z"
-        stroke={"currentColor"}
+        stroke={fill || hover ? "#F31260" : "#9ba1a6"}
         strokeWidth={1.5}
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -89,8 +93,10 @@ export default function Post({
         </Card.Body>
         <Card.Footer>
           <Text css={{ color: "$accents8" }}>{comments.length} comments</Text>
+          <Spacer x={0.5} />
           <HeartIcon fill={currentUserLiked} onClick={() => addLike(id)} />
-          <Text>{likes.length}</Text>
+          <Spacer x={0.1} />
+          <Text color={currentUserLiked ? "#F31260" : "#9ba1a6"}>{likes.length}</Text>
         </Card.Footer>
       </Card>
       <Spacer y={1} />
