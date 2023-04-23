72318914b08b9431d09c73dd63667d45193aea78
Yaseen Mustapha <yaseenmust@gmail.com>
2023-04-22 23:31:22 -0500

    feat: enhance post message with OpenAI GPT-3.5

--- app/posts/AddPost.tsx
+++ app/posts/AddPost.tsx
@@ -19,12 +19,39 @@ export default function AddPost(): JSX.Element {
   const { data: session } = useSession();
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
+  const [enhancing, setEnhancing] = useState(false);
+  const [enhanceError, setEnhanceError] = useState(false);
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("Error creating post.");
   const router = useRouter();
 
+  const enhanceWithAI = async () => {
+    if (enhancing) return;
+    setEnhancing(true);
+    setEnhanceError(false);
+
+    const res = await fetch("/api/enhanceMessage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: content,
+      }),
+    });
+
+    if (res.ok) {
+      const GPTdata = await res.json();
+      setContent(GPTdata.content);
+    } else {
+      setEnhanceError(true);
+    }
+    setEnhancing(false);
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    if (loading) return;
     setLoading(true);
     setError(false);
 
@@ -74,10 +101,15 @@ export default function AddPost(): JSX.Element {
         onChange={(e) => setContent(e.target.value)}
       />
       <Spacer y={0.5} />
-      <Row wrap="wrap" align="center" justify="space-between">
+      <Row wrap="wrap" align="center">
         <Popover isOpen={error} onOpenChange={setError} placement="right">
           <Popover.Trigger>
-            <Button type="submit" disabled={!session}>
+            <Button
+              type="submit"
+              disabled={!session}
+              auto
+              style={{ minWidth: "100px" }}
+            >
               {loading ? <Loading color="currentColor" size="sm" /> : "Post"}
             </Button>
           </Popover.Trigger>
@@ -86,7 +118,30 @@ export default function AddPost(): JSX.Element {
           </Popover.Content>
         </Popover>
 
-        <Text>{content.length}/300</Text>
+        <Popover isOpen={enhanceError} onOpenChange={enhanceError ? setEnhanceError : undefined} placement="right">
+          <Popover.Trigger>
+            <Button
+              color="gradient"
+              disabled={!session}
+              style={{ marginLeft: 10, minWidth: "150px" }}
+              onPress={enhanceWithAI}
+              auto
+            >
+              {enhancing ? (
+                <Loading type="points-opacity" color="currentColor" size="sm" />
+              ) : (
+                "Enhance with AI 😎"
+              )}
+            </Button>
+          </Popover.Trigger>
+          <Popover.Content>
+            <Text css={{ p: "$10" }}>Error enhancing post. Please try again.</Text>
+          </Popover.Content>
+        </Popover>
+
+        <Text style={{ marginLeft: "auto" }}>
+          {(content && content.length) || "0"}/300
+        </Text>
       </Row>
 
       <Spacer y={2} />
