66ab2111dbad24b0a8bad142a02394621f182082
Yaseen Mustapha <yaseenmust@gmail.com>
2023-08-10 18:12:16 -0500

    feat: upgrade NextUI to V2 and fix Nav

--- app/ProvidersWrapper.tsx
+++ app/ProvidersWrapper.tsx
@@ -1,21 +1,21 @@
 "use client";
-import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-const lightTheme = createTheme({
-  type: "light",
-});
+// const lightTheme = createTheme({
+//   type: "light",
+// });
 
-const darkTheme = createTheme({
-  type: "dark",
-  theme: {
-    colors: {
-      gradient:
-        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
-    },
-  },
-});
+// const darkTheme = createTheme({
+//   type: "dark",
+//   theme: {
+//     colors: {
+//       gradient:
+//         "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
+//     },
+//   },
+// });
 
 export default function ProvidersWrapper({
   children,
@@ -27,10 +27,10 @@ export default function ProvidersWrapper({
       <NextThemesProvider
         defaultTheme="system"
         attribute="class"
-        value={{
-          light: lightTheme.className,
-          dark: darkTheme.className,
-        }}
+        // value={{
+        //   light: lightTheme.className,
+        //   dark: darkTheme.className,
+        // }}
       >
         <NextUIProvider>{children}</NextUIProvider>
       </NextThemesProvider>
