250b13e0bb6df157d057d7344edff74d32ce1c36
Yaseen Mustapha <yaseenmust@gmail.com>
2023-08-14 19:29:09 -0500

    fix: revert server-side fetch behavior

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,6 +1,13 @@
-import { getSinglePost } from "@/lib/posts";
 import { Comment, Like, User } from "@prisma/client";
 import Thread from "./Thread";
+
+async function getPost(postId: string) {
+  const res = await fetch(`${process.env.BASE_URL}/api/posts/${postId}`, {
+    cache: "no-store",
+  });
+  // console.log(res);
+  return res.json();
+}
 
 export default async function PostDetails({
   params,
@@ -11,10 +18,15 @@ export default async function PostDetails({
     id: string;
     content: string;
     user: User;
-    createdAt: Date;
+    createdAt: string;
     likes: Like[];
-    comments: (Comment & { user: User })[];
-  } | null = await getSinglePost(params.postId);
+    comments: {
+      id: string;
+      user: User;
+      createdAt: string;
+      content: string;
+    }[];
+  } | null = await getPost(params.postId);
 
   if (data === null) {
     // Handle the case where data is null, such as showing an error message
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,16 +1,28 @@
-import { getAllPosts } from "@/lib/posts";
-import { Like, Comment, User } from "@prisma/client";
+import { Comment, Like, User } from "@prisma/client";
 import Feed from "./Feed";
+
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`, {
+    cache: "no-store",
+  });
+  // console.log(res);
+  return res.json();
+}
 
 export default async function Posts() {
   const data: {
     id: string;
     content: string;
     user: User;
-    createdAt: Date;
+    createdAt: string;
     likes: Like[];
-    comments: Comment[];
-  }[] = await getAllPosts();
+    comments: {
+      id: string;
+      user: User;
+      createdAt: string;
+      content: string;
+    }[];
+  }[] = await getPosts();
   // console.log(data);
   return (
     <main>
